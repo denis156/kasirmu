b6dcb48b99cd447eb505f817b6d18100efe16628
Denisa_13 <56766058+denis156@users.noreply.github.com>
2025-08-18 04:56:25 +0800

    refactor(security): remove upgrade-insecure-requests csp meta tag

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,7 +5,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, viewport-fit=cover">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <title>{{ isset($title) ? $title . ' - ' . config('app.name') : config('app.name') }}</title>
     <link rel="shortcut icon" href="{{ asset('favicon.png') }}">
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, viewport-fit=cover">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <title>{{ config('app.name', 'KasirMu By ArteliaDev') }} - Sistem POS Modern</title>
     <link rel="shortcut icon" href="{{ asset('favicon.png') }}">
 
